44012eafe8339f2857c2066e978ccf52bcb1ac6b
kunalsonawane730 <95461743+kunalsonawane730@users.noreply.github.com>
2022-02-03 10:13:56 +0530

    [kunal]commiting the tic tac toe program

---
+++ TikTacToe/TikTacToe/TicTacToe.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TikTacToe
+{
+    public class TicTacToeGame
+    {
+        public void board()
+        {
+            char[] board = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+            Console.WriteLine("Welcome to Tic Tac Toe Game");
+        }
+    }
+}
