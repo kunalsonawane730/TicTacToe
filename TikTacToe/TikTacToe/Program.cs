11b3e31269da9fa2b5c78705e356e90301a63802
kunalsonawane730 <95461743+kunalsonawane730@users.noreply.github.com>
2022-02-03 10:21:16 +0530

    [kunal]commiting UC1

--- TikTacToe/TikTacToe/Program.cs
+++ TikTacToe/TikTacToe/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using TikTacToe;
+// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+TicTacToeGame tictactoe = new TicTacToeGame();
+tictactoe.board();
