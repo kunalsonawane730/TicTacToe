454b38fa0040ccdb29e07aed79054450906e7b70
kunalsonawane730 <95461743+kunalsonawane730@users.noreply.github.com>
2022-02-03 11:39:32 +0530

    [kunal]commiting UC2

--- TikTacToe/TikTacToe/TicTacToe.cs
+++ TikTacToe/TikTacToe/TicTacToe.cs
@@ -8,10 +8,39 @@ namespace TikTacToe
 {
     public class TicTacToeGame
     {
+        public int player = 1;
+        public int choice;
+        public int flag = 0;
         public void board()
         {
             char[] board = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
-            Console.WriteLine("Welcome to Tic Tac Toe Game");
+            Console.WriteLine("Player1:X and Player2:O");
+            Console.WriteLine("\n");
+            if (player % 2 == 0)//checking the chance of the player
+            {
+                Console.WriteLine("Player 2 Chance");
+            }
+            else
+            {
+                Console.WriteLine("Player 1 Chance");
+            }
+            Console.WriteLine("\n");
+             choice = int.Parse(Console.ReadLine());
+
+            if (board[choice] != 'X' && board[choice] != 'O')
+            {
+                if (player % 2 == 0) //if chance is of player 2 then mark O else mark X
+                {
+                    board[choice] = 'O';
+                    player++;
+                }
+                else
+                {
+                    board[choice] = 'X';
+                    player++;
+
+                }
+            }
         }
     }
 }
